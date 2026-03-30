9ec80c4f603d37820dc2f503eba1843fc98dab42
JASPREET SHARMA <jaspreetsharma553@gmail.com>
2026-03-30 12:48:03 +0530

    Refactor email configuration to use environment variables

--- src/common/config/email.js
+++ src/common/config/email.js
@@ -1,10 +1,12 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: "cec231163.cse.cec@cgc.edu.in",
-    pass: process.env.GOOGLE_APP_PASSWORD, // The 16-character App Password
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    secure: false,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
   },
 });
 const sendMail = async (to, subject, html) => {
